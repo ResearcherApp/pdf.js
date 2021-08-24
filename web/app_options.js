ae5db3b9eb4ac00ed29bff435a8b44ccfc4254b0
Jon Baker <jonty800@gmail.com>
2021-08-24 11:34:28 +0100

    Disable sidebar load

--- web/app_options.js
+++ web/app_options.js
@@ -152,7 +152,7 @@ const defaultOptions = {
   },
   sidebarViewOnLoad: {
     /** @type {number} */
-    value: -1,
+    value: 0,
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE,
   },
   scrollModeOnLoad: {
